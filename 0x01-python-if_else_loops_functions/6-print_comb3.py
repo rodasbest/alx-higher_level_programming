e7bfd2521f1a4f2230f5ed5d4358233db0c28b77
Rodas <rodasbest@gmail.com>
2022-11-30 21:09:18 +0000

    executable file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(0, 10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
-	else:
+        else:
             print("{}{}".format(i, j), end=", ")
