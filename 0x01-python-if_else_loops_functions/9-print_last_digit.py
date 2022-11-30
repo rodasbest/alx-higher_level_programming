f9adb482417553f62955299437514fe0b534cb9e
Rodas <rodasbest@gmail.com>
2022-11-30 21:17:14 +0000

    executable file

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,5 @@
 
 
 def print_last_digit(number):
-    if number < 0:
-        number = abs(number)
-    print('{}'.format(number % 10) end='')
-    return number % 10
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
