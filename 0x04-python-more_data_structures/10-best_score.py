1b66ab222d91cf340d0ac176d4a96f0ad9b8f0ce
Rodas <rodasbest@gmail.com>
2022-12-07 19:07:41 +0000

    another approach

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) is 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0
         return None
-    one_loop = True
-    for people in a_dictionary:
-        if one_loop:
-            best = people
-            score = a_dictionary[people]
-            one_loop = False
-        if a_dictionary[people] > score:
-            score = a_dictionary[people]
-            best = people
-    return best
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
