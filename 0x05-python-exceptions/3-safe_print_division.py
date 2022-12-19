f863351465c25969230aea0f767af0973fa03e36
Rodas <rodasbest@gmail.com>
2022-12-19 20:23:44 +0000

    executable file

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    c = None
     try:
-        result = a/b
-        return result
+        c = a / b
     except:
-        result = None
-        return result
+        pass
     finally:
-        print('Inside result: {}'.format(result))
+        print("Inside result: {}".format(c))
+        return c
