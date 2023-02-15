c0d477d9d403197b435ec94a3615b621f35ea34d
Rodas <rodasbest@gmail.com>
2023-02-15 21:26:46 +0000

    inserted forgotten ;

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 -- creates the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates a user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
 -- grants SELECT privileges to a user
